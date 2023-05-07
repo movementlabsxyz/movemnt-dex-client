0a3b0057e797f6302944f7b04671855f7d56d00a
jason <jason@hedmans.org>
2023-05-07 21:26:10 +0200

    add active link styles

--- components/Navbar/NavLink.tsx
+++ components/Navbar/NavLink.tsx
@@ -3,20 +3,27 @@ import React from 'react';
 import {NavLink as NavLinkType} from "@/types/NavLink";
 import {Button} from "@chakra-ui/react";
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 interface Props {
     link: NavLinkType
 }
 
 const NavLink: React.FC<Props> = ({ link }) => {
+
+    const router = useRouter();
+
+    const isActive = router.asPath === link.path;
+
     return (
         <Link
             href={link.path}
             passHref
         >
             <Button
-                variant='ghost'
+                variant={isActive ? 'solid' : 'ghost'}
                 leftIcon={<link.icon />}
+                colorScheme={isActive ? 'brand' : undefined}
             >
                 {link.name}
             </Button>
